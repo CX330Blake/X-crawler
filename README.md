9b807dcad08552cb2ff71099892831d8a022207c
DESKTOP-COR4OEI\陳子雋 <timchen975@gmail.com>
2024-01-25 15:29:43 +0800

    2024/01/25

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # X-crawler
+
+## Overview
+
+This Python script utilizes Selenium and BeautifulSoup to crawl tweets from Twitter(now X) based on a given keyword and date range. The scraped data can be saved in both text and JSON formats.
+
+## Requirements
+
+- Python 3.x
+- Install required packages by running: `pip install -r requirements.txt`
+
+
--- crawler.py
+++ crawler.py
@@ -9,27 +9,33 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 import json
+import os
+
 
 def debug():
     with open("test.html", "w", encoding="utf-8") as file:
         file.write(driver.page_source)
 
-def parse_count_string(count_str):
+
+def parse_count_string(count_str) -> int:
     multipliers = 1
     if "K" in count_str:
         multipliers = 1000
     elif "M" in count_str:
         multipliers = 1000000
-    count_str = count_str.replace('K', '').replace('M', '')
+    count_str = count_str.replace("K", "").replace("M", "")
 
     try:
-        count = int(float(count_str) * multipliers) 
+        count = int(float(count_str) * multipliers)
         return count
     except ValueError:
         return None
 
+
 def get_tweets_txt(path):
-    path += ".txt"
+    path += f"{keyword.replace(" ", " - ")}_{start_date}_{end_date}.txt".replace(
+        " ", ""
+    )
     flag = 0
     tweet_count = 0
     id_set = []
@@ -76,67 +82,84 @@ def get_tweets_txt(path):
         if current_time - last_write_time > timeout:
             flag = 1
 
-def get_tweets_json(path):
-    path += ".json"
+
+def get_tweets_json(path: str, timeout: int):
+    path += f"{keyword.replace(" ", " - ")}_{start_date}_{end_date}.json".replace(
+        " ", ""
+    )
     flag = 0
     id_set = []
-    timeout = 5
+    timeout = timeout
     last_write_time = time.time()
     tweets_data_list = []
-    
+
     while True:
         print(f"Found {len(tweets_data_list)} tweet(s) currently.")
         current_time = time.time()
         articles = driver.find_elements(By.TAG_NAME, "article")
-        
+
         for article in articles:
-            data = {
-                "text": "",
-                "likes": 0,
-                "replies": 0,
-                "retweets": 0  
-            }
+            data = {"text": "", "likes": 0, "replies": 0, "retweets": 0}
             try:
-                tweet = article.find_element(By.CSS_SELECTOR, '[data-testid="tweetText"]')
+                tweet = article.find_element(
+                    By.CSS_SELECTOR, '[data-testid="tweetText"]'
+                )
             except:
                 continue
             spans = tweet.find_elements(By.TAG_NAME, "span")
             id = tweet.get_attribute("id")
-            
+
             for span in spans:
-                if "r-qvk6io" not in span.get_attribute("class") and "r-lrvibr" not in span.get_attribute("class"):
+                if "r-qvk6io" not in span.get_attribute(
+                    "class"
+                ) and "r-lrvibr" not in span.get_attribute("class"):
                     data["text"] += span.text.replace("\n", "")
-                    
+
             likes = article.find_element(By.CSS_SELECTOR, '[data-testid="like"]').text
-            replies = article.find_element(By.CSS_SELECTOR, '[data-testid="reply"]').text
-            retweets = article.find_element(By.CSS_SELECTOR, '[data-testid="retweet"]').text
-            
-            data["likes"] = parse_count_string(likes)
-            data["replies"] = parse_count_string(replies)
-            data["retweets"] = parse_count_string(retweets)
-            
+            replies = article.find_element(
+                By.CSS_SELECTOR, '[data-testid="reply"]'
+            ).text
+            retweets = article.find_element(
+                By.CSS_SELECTOR, '[data-testid="retweet"]'
+            ).text
+            if likes:
+                data["likes"] = parse_count_string(likes)
+            else:
+                data["likes"] = 0
+            if replies:
+                data["replies"] = parse_count_string(replies)
+            else:
+                data["replies"] = 0
+            if retweets:
+                data["retweets"] = parse_count_string(retweets)
+            else:
+                data["retweets"] = 0
+
             if id not in id_set:
                 id_set.append(id)
                 tweets_data_list.append(data)
                 last_write_time = time.time()
-        
+
         if flag == 1:
             print(f"Found {len(tweets_data_list)} tweet(s) FINALLY.")
             break
-        
+
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, "article"))
         )
-        
+
         body = driver.find_element(By.TAG_NAME, "body")
         body.send_keys(Keys.PAGE_DOWN)
         time.sleep(0.5)
-        
+
         if current_time - last_write_time > timeout:
             flag = 1
-    tweets_dict = {f"tweet_{i + 1}": tweet_data for i, tweet_data in enumerate(tweets_data_list)}
+    tweets_dict = {
+        f"tweet_{i + 1}": tweet_data for i, tweet_data in enumerate(tweets_data_list)
+    }
     with open(path, "w", encoding="utf-8") as file:
         json.dump(tweets_dict, file, ensure_ascii=False, indent=4)
+
 
 my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 chrome_options = webdriver.ChromeOptions()
@@ -150,7 +173,7 @@ email = "AN4126068@gs.ncku.edu.tw"
 username = "ccep_an4126068"
 password = "an4126068"
 
-keyword = "tsla"
+keyword = "tsla elon musk"
 start_date = "2020-01-01"
 end_date = "2022-01-01"
 # keyword = input("請輸入關鍵字(以空格分隔): ")
@@ -218,10 +241,11 @@ WebDriverWait(driver, 10).until(
 
 
 while True:
+    dir_path = "./Data/"
     try:
-        get_tweets_json(
-            f"./Data/{keyword.replace(" ", " - ")}_{start_date}_{end_date}".replace(" ", "")
-        )
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        get_tweets_json(dir_path, 10)
         break
     except Exception as e:
         print(f"{e}")
